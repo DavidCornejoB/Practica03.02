292988092e0cf0c00b94b44a5e1b1deba9331db6
stali <stali@STALIN>
2018-01-10 14:30:02 -0500

    Falta Ordenamiento

--- Practica03_02/src/practica03_02/Principal1.java
+++ Practica03_02/src/practica03_02/Principal1.java
@@ -14,52 +14,58 @@ public class Principal1 {
 
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
-        
-        Map<Integer,Persona>mapa=new HashMap<>();
-        ArrayList<Casa>lista=new ArrayList<>();
-        Diccionario dic=new Diccionario(mapa);
-        Lista listaLib=new Lista(lista);
-        Persona p=new Persona();
-        Casa c=new Casa();
-        int op;
-        System.out.println("***Ingresar");
-        System.out.println("Cuantos elementos desea ingresar?");
-        int elem = leer.nextInt();
-        leer.nextLine();
-        
-        for(int i = 0; i<elem; i++){
-            
-            System.out.println("Nombre de la persona");
-            String nombre=leer.next();            
-            int codPersona=i+1;
-            JOptionPane.showMessageDialog(null,"ID Propietario Generada","Codigo Propietario",JOptionPane.INFORMATION_MESSAGE);
+
+        Map<Integer, Persona> mapa = new HashMap<>();
+        ArrayList<Casa> lista = new ArrayList<>();
+        Diccionario dic = new Diccionario(mapa);
+        Lista listaLib = new Lista(lista);
+        Persona p = new Persona();
+        Casa c = new Casa();
+        int op, opCasa, elem;
+        System.out.println("***Ingreso de datos***");
+        elem = Integer.parseInt(JOptionPane.showInputDialog(null, "Cuantos elementos desea ingresar", "Ingreso", JOptionPane.NO_OPTION));
+
+        for (int i = 0; i < elem; i++) {
+
+            System.out.println("Nombre del propietario");
+            String nombre = leer.next();
+            int codPersona = i + 1;
+            JOptionPane.showMessageDialog(null, "ID Propietario Generada", "Codigo Propietario", JOptionPane.INFORMATION_MESSAGE);
             p.setId(codPersona);
             p.setNomApe(nombre);
-           
-            System.out.println("Nombre de la casa de: "+nombre);
-            String NomCasa=leer.next();
-            int codCasa=i+1;
-            JOptionPane.showMessageDialog(null,"ID Casa Generada","Codigo Casa",JOptionPane.INFORMATION_MESSAGE);
+
+            System.out.println("Nombre de la casa de: " + nombre);
+            String NomCasa = leer.next();
+            int codCasa = i + 1;
+            JOptionPane.showMessageDialog(null, "ID Casa Generada", "Codigo Casa", JOptionPane.INFORMATION_MESSAGE);
             c.setId(codCasa);
             c.setNombre(NomCasa);
             p.setCasa(c);
-            
+
             dic.agregarElemento(codPersona, p.getNomApe());
             listaLib.agregarElemento(c.getNombre());
         }
-       
-        op=Integer.parseInt(JOptionPane.showInputDialog(null, "Codigo del propietario", "Eliminar",JOptionPane.NO_OPTION));
-        Object elemento=dic.recuperarElemento(op);
-        System.out.println(elemento);
-        op=Integer.parseInt(JOptionPane.showInputDialog(null, "Codigo del propietario", "Eliminar",JOptionPane.NO_OPTION));
+        op = Integer.parseInt(JOptionPane.showInputDialog(null, "Codigo del propietario", "Recuperar", JOptionPane.NO_OPTION));
+        Object elemento = dic.recuperarElemento(op);
+        System.out.println("Recuperado "+elemento);
+        op = Integer.parseInt(JOptionPane.showInputDialog(null, "Codigo del propietario", "Eliminar", JOptionPane.NO_OPTION));
         dic.eliminarElemento(op);
+        listaLib.eliminarElemento(op-1);
         System.out.println("***Listado de las propietarios ingresados");
-        List lista1=dic.recuperarElementos();
-        for(int i=0;i<lista1.size();i++){
+        List lista1 = dic.recuperarElementos();
+        for (int i = 0; i < lista1.size(); i++) {
             System.out.println(lista1.get(i));
         }
-        
-        
+
+        opCasa = Integer.parseInt(JOptionPane.showInputDialog(null, "Codigo de la casa", "Recuperar", JOptionPane.NO_OPTION));
+        Object elemento1 = listaLib.recuperarElemento(opCasa-1);
+        System.out.println("Recuperado "+elemento1);
+        opCasa = Integer.parseInt(JOptionPane.showInputDialog(null, "Codigo de la casa", "Eliminar", JOptionPane.NO_OPTION));
+        listaLib.eliminarElemento(opCasa-1);
+        //Listar casas
+        for(int j=0;j<lista.size();j++){
+            System.out.println(lista.get(j));
+        }
     }
-    
+
 }
